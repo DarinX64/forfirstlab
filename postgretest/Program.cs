a15017b3afc78ebaf12f8cef1b0bec700ad67101
UserFromLinux <darin.x64@gmail.com>
2021-11-28 14:30:04 +0200

    Project initialized

---
+++ postgretest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+
+namespace postgretest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = "Host=localhost;Username=postgres;Password=230101;Database=postgres";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+            Console.Read();
+        }
+    }
+}
